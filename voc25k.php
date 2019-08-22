973bd53c8ddd9d15f25f3ad80bb637da51c1e605
diffamr <44928835+diffamr@users.noreply.github.com>
2019-08-22 15:08:50 +0700

    Add files via upload

---
+++ voc25k.php
@@ -0,0 +1,9 @@
+<?php
+
+echo " SNIFF VOC CASHBACK 90% MAX 25K \n\n";
+echo "[1]  No. Handphone : 6282282054477 \n";
+echo "[2]  OTP Guys      : 6294 \n";
+echo "[3]  TUNGGU SEBENTAR SLURR...! \n";
+echo "[4]  BERHASIL MENDAPATKAN VOC! \n";
+
+?>
